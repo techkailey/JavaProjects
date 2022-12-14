ecef50245912613a046fe86c2cf6a889c826da83
kaberge3 <kaberge3@uno.edu>
2022-12-14 11:35:06 -0600

    Virtual Pet

---
+++ VirtualPet/README.java
@@ -0,0 +1,18 @@
+/** Kailey Bergeron 
+Homework 3
+
+For my program I used a puplic class and imported Scanner for user input.
+
+The program is a Virtual Pet game.
+In the beginning of the program, the user will be asked to input a name for the pet.
+Then they have to take care of the pet. After a certain amount of days the values of the attributes 
+(dirtiness, boredom, and hunger) will decrease, resulting in the game to be more of a challenge. If 
+the values reach below a certain value the pet will run away. The user is told what 
+values are low to prevent the puppy from running away. They are also informed that because 
+they puppy has reached a certain age they will need to do more for it. If the puppy does 
+run away, a reason will be given. 
+
+There are no bugs that I have found. 
+**/
+
+
